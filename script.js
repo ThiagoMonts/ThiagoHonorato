c5dafebe2b9918eca2f9fa3ad85706e344608b94
ThiagoMonts <thiagohonorato@hotmail.com>
2022-10-14 12:08:44 -0300

    Implementação de temas

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function toggleMenu() {
 }
 
 burguer.addEventListener('click', toggleMenu);
+
+const chk = document.getElementById('chk')
+
+chk.addEventListener('change', () => {
+    document.body.classList.toggle('dark')
+})
+
+
