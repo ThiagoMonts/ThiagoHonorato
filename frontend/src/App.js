98eae43fdfeac3f8b5154d060c079d6c7c2aef05
ThiagoMonts <thiagohonorato@hotmail.com>
2023-03-23 13:06:26 -0300

    3.0 - Criação da estrutura de pastas e arquivos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
+import './App.scss';
 
 function App() {
   return (
-    <div>
-      App
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
     </div>
   );
 }
