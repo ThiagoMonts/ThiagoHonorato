aec2d47cf616fc02995b8a4e8363e7c9b11de86d
ThiagoMonts <thiagohonorato@hotmail.com>
2023-04-04 12:16:22 -0300

    Correção do layout

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -35,20 +35,22 @@ const Header = () => (
           /></h1>
         <p className='p-text'>Sou Desenvolvedor Front-end</p> <br />
         <p className='p-text'>No momento estou no 3º período do curso de Análise e Desenvolvimento de Sistemas.</p>
-
-        <div>
+      </div>
+      <div className='app__header-info-button app_flex'>
+        <div className='app__header-info-button1'>
           <a href="https://github.com/ThiagoMonts" target='_blank'>
             <button type='button'>
               <FaGithub /> <p>Github</p>
             </button>
           </a>
+        </div>
+        <div className='app__header-info-button2'>
           <a href="https://github.com/ThiagoMonts" target='_blank'>
             <button type='button'>
               <FaCloudDownloadAlt /> <p>Currículo</p>
             </button>
           </a>
         </div>
-
 
       </div>
     </motion.div>
